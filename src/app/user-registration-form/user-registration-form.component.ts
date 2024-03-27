9b39ad00a74054731db2c3877be97f75dcd12802
nesty11 <arielvale8@hotmail.com>
2024-03-26 21:51:43 -0400

    implemented user registration functionality

---
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { UserRegistrationService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-registration-form',
+  templateUrl: './user-registration-form.component.html',
+  styleUrls: ['./user-registration-form.component.scss'],
+})
+export class UserRegistrationFormComponent implements OnInit {
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+
+  constructor(
+    public UserRegistrationService: UserRegistrationService,
+    public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+    public snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {}
+
+  //This is the fuction responsible for sending the form inputs to the backend
+  registerUser(): void {
+    this.UserRegistrationService.userRegistration(this.userData).subscribe(
+      (result) => {
+        //Logic for a successful user registration goes here. (To be implemented)
+        this.dialogRef.close(); //This will close the modal on success
+        this.snackBar.open(result, 'OK', {
+          duration: 2000
+        });
+      },
+      (result) => {
+        this.snackBar.open(result, 'OK', {
+          duration: 2000
+        });
+      }
+    );
+  }
+}
