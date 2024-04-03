bd0ec2c4e2ae2d506d7d59dd959ff458c456b13e
nesty11 <arielvale8@hotmail.com>
2024-04-02 23:35:50 -0400

    removed stringify on token
    token is already being stringify so no need to stringify again.
    added angular router

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { UserRegistrationService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login-form',
@@ -14,7 +15,8 @@ export class UserLoginFormComponent implements OnInit {
   constructor(
     public UserRegistrationService: UserRegistrationService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar
+    public snackBar: MatSnackBar,
+    public router: Router
   ) {}
 
   ngOnInit(): void {}
@@ -23,11 +25,13 @@ export class UserLoginFormComponent implements OnInit {
     this.UserRegistrationService.userLogin(this.userData).subscribe(
       (result) => {
         localStorage.setItem('user', JSON.stringify(result.user));
-        localStorage.setItem('token', JSON.stringify(result.token));
+        localStorage.setItem('token', result.token);
         this.dialogRef.close();
-        this.snackBar.open("Login Successful", 'OK', {
+        this.snackBar.open('Login Successful', 'OK', {
           duration: 2000,
         });
+
+        this.router.navigate(['movies']);
       },
       (result) => {
         this.snackBar.open(result, 'OK', {
