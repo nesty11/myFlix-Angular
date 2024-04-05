6d1d4af6e8192088c74260a13397c48b3fa5a4c3
nesty11 <arielvale8@hotmail.com>
2024-04-04 21:13:36 -0400

    adding synopsis component functionality

--- src/app/synopsis/synopsis.component.ts
+++ src/app/synopsis/synopsis.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-synopsis',
   templateUrl: './synopsis.component.html',
-  styleUrls: ['./synopsis.component.scss']
+  styleUrls: ['./synopsis.component.scss'],
 })
-export class SynopsisComponent {
+export class SynopsisComponent implements OnInit {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { Description: string }) {}
 
+  ngOnInit(): void {}
 }
