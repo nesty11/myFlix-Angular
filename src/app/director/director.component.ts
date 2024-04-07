ce5d945e84087afa46453c929fcdaf30abde7d38
nesty11 <arielvale8@hotmail.com>
2024-04-07 13:36:08 -0400

    component extracts director information

--- src/app/director/director.component.ts
+++ src/app/director/director.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-director',
   templateUrl: './director.component.html',
-  styleUrls: ['./director.component.scss']
+  styleUrls: ['./director.component.scss'],
 })
-export class DirectorComponent {
+export class DirectorComponent implements OnInit {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { director: any }) {}
 
+  ngOnInit(): void {}
 }
