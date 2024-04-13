a2cec59a1a0e380f8242f03072961db4dc3a7146
nesty11 <arielvale8@hotmail.com>
2024-04-12 20:07:26 -0400

    can now update all user credentials

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -9,7 +9,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./user-profile.component.scss'],
 })
 export class UserProfileComponent implements OnInit {
-  userData: any = {}; // Initialize userData object
+  userData: any = {};
+  newPassword: string = '';
 
   constructor(
     public userRegistrationService: UserRegistrationService,
@@ -18,38 +19,39 @@ export class UserProfileComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.getUserData(); // Call getUserData when component initializes
+    this.getUserData();
   }
 
   getUserData(): void {
-    const username = localStorage.getItem('user'); // Assuming you store the username in localStorage upon login
+    const username = localStorage.getItem('user');
     if (username) {
       this.userRegistrationService.getUser(username).subscribe(
         (result) => {
-          // Populate userData object with fetched user data
           this.userData = result;
-          console.log('User Data:', this.userData); // Log userData
         },
         (error) => {
           console.error('Error fetching user data:', error);
-          // Handle error (e.g., display error message)
           this.snackBar.open('Failed to fetch user data', 'OK', {
             duration: 2000,
           });
         }
       );
     } else {
-      // Handle case where username is not available in localStorage
       console.error('Username not found in localStorage');
-      // Redirect user to login or handle appropriately
     }
   }
 
   editUser(): void {
-    console.log('Updated User Data:', this.userData); // Log updated userData before making the request
+    const currentUser = JSON.parse(localStorage.getItem('user') || '{}');
+
+    this.userData.Username = currentUser.Username;
+
+    if (this.newPassword) {
+      this.userData.Password = this.newPassword;
+    }
+
     this.userRegistrationService.editUser(this.userData).subscribe(
       (result) => {
-        // Update userData with the response
         this.userData = result;
         this.snackBar.open('User update successful', 'OK', {
           duration: 2000,
